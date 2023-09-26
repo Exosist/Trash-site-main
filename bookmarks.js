73ad2ca9b622974a1abf03d0d80026a357374ee6
Exosist <Jimmieleifandersson@gmail.com>
2023-09-26 20:31:59 +0200

    Fixed Weather

---
+++ bookmarks.js
@@ -0,0 +1,24 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Mail/Important",
+    links: [
+      { name: "Inbox", url: "https://mail.google.com" },
+      {name: "Arbetsförnedringen", url: "https://arbetsformedlingen.se"},
+      {name: "LinkedIN", url: "https://linkedin.com"},
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "SVT Play", url: "https://www.svtplay.se/" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Dagens Nyheter", url: "https://dn.se" },
+      { name: "Viaplay", url: "https://viaplay.se"},
+       { name: "Twitch",
+        url: "https://www.twitch.tv/directory/following/live",
+      },
+    ],
+  },
+];
